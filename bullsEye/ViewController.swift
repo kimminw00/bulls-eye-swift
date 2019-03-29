654bdf869721528f08df5c83323b05d25f4ea280
JoeyValentine <rlaalsdn0506@naver.com>
2019-03-30 05:26:38 +0900

    Polishing the game

--- bullsEye/ViewController.swift
+++ bullsEye/ViewController.swift
@@ -11,9 +11,13 @@ import UIKit
 class ViewController: UIViewController {
     var currentValue: Int = 50
     var targetValue = 0
+    var score = 0
+    var round = 0
     
     @IBOutlet weak var targetLabel: UILabel!
     @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var roundLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,8 +26,30 @@ class ViewController: UIViewController {
     }
 
     @IBAction func showAlert(_ sender: UIButton) {
-        let message = "The value of the slider is: \(currentValue)" + "\nThe target value is: \(targetValue)"
-        let alert = UIAlertController(title: "Hello World", message: message, preferredStyle: .alert)
+        let difference = abs(targetValue - currentValue)
+        var points = 100 - difference
+        let title: String
+        if difference == 0 {
+            title = "Perfect!"
+            points += 100
+        }
+        else if difference < 5 {
+            title = "You almost had it"
+            if difference == 1 {
+                points += 50
+            }
+        }
+        else if difference < 10 {
+            title = "Pretty good!"
+        }
+        else {
+            title = "Not even close..."
+        }
+
+        score += points
+        
+        let message = "You scored \(points) points"
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
         let action = UIAlertAction(title: "O.K", style: .default, handler: nil)
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
@@ -37,9 +63,12 @@ class ViewController: UIViewController {
     
     func updateLabels() {
         targetLabel.text = String(targetValue)
+        scoreLabel.text = String(score)
+        roundLabel.text = String(round)
     }
     
     func startNewRound() {
+        round += 1
         targetValue = Int.random(in:1...100)
         currentValue = 50
         slider.value = Float(currentValue)
